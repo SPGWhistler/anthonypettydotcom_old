8954e380c48a3bd80c8e4d1188442bea968eeebe
Tony Petty <tpetty@appmobi.com>
2013-07-12 13:15:35 -0400

    Working on presentation.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 $(function(){
+	$('#menu ul').css('margin-top', -($('#menu ul').height() / 2));
+	$('#menu ul').fadeIn(1500);
 	$('#menu a').click(function(e){
 		if ($(this).attr('href').substring(0,1) === '#') {
 			e.preventDefault();
 			var div = $(this).attr('href');
 			var $cur = $('#content div:visible');
 			if ('#' + $cur.attr('id') !== div) {
-				$cur.slideUp();
-				$(div).slideDown();
+				$cur.fadeOut(100, function(){
+					$(div).css('margin-top', -($(div).height() / 2));
+					$(div).fadeIn(100);
+				});
 			}
 			return false;
 		}
