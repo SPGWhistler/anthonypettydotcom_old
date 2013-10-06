f965d8e66d46ae442062c5e770eef95701be0908
Tony Petty <tpetty@appmobi.com>
2013-10-06 06:11:33 -0400

    Added resume and made copy and js changes.

--- js/index.js
+++ js/index.js
@@ -11,22 +11,23 @@
 					$('#headernav li').removeClass('active');
 					$this.parent().addClass('active');
 					$('.container>div').hide();
-					$(href).show();
+					$(href).fadeIn();
 				});
 			}
 		});
 		$('a.navbar-brand').click(function () {
 			$('#headernav li a[href="#hello"]').click();
 		});
+		$('nav').fadeIn();
 		loc = window.location.hash;
 		if (loc !== "") {
 			$('#headernav li').removeClass('active');
 			$('#headernav li a[href="' + loc + '"]').parent().addClass('active');
 			$('.container>div').hide();
-			$(loc).show();
+			$(loc).fadeIn(1200);
 		} else {
 			$('#headernav li a[href="#home"]').parent().addClass('active');
-			$('#hello').show();
+			$('#hello').fadeIn(1200);
 		}
 	});
 }());
